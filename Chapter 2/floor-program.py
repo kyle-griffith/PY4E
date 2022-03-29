c35c29272d50b24ec0ce653f4208d07b956a08b1
Kyle Griffith <kylegriffithdev@gmail.com>
2022-03-29 17:24:24 -0500

    Variables, Conditionals, Expressions, If/Else

---
+++ Chapter 2/floor-program.py
@@ -0,0 +1,8 @@
+# Create a program for determining the current floor you are on if you were in Europe
+
+# Declaring the variable that holds our initial input / prompt
+inp = input('Europe floor? ')
+# Declaring the US variable, and converting the string recieved from the input / prompt into an integer to interact with our + 1
+usf = int(inp) + 1
+# Returning the information recieved from the input
+print('US floor', usf)
---
+++ Chapter 2/variables.py
@@ -0,0 +1,3 @@
+first_name = 'Kyle'
+last_name = 'Griffith'
+print(first_name + ' ' + last_name)
